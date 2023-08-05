a1ae2125a93d90bd5f55523e6326af9d9f64a903
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-08-05 22:02:42 +0900

    refactor: 테스트 컨트롤러 제거

--- src/main/java/com/cstapin/auth/ui/AuthController.java
+++ src/main/java/com/cstapin/auth/ui/AuthController.java
@@ -6,7 +6,10 @@ import com.cstapin.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -39,9 +42,4 @@ public class AuthController {
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/api/v1/admin/test")
-    public ResponseEntity<TokenResponse> test() {
-
-        return null;
-    }
 }
