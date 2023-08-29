4a32d92e57479f0461ea95d3977655795623a520
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-08-29 22:12:29 +0900

    test: 문제 상세 조회 문서 테스트 구현

--- src/test/java/com/cstapin/documentation/QuizDocumentation.java
+++ src/test/java/com/cstapin/documentation/QuizDocumentation.java
@@ -9,8 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-import static com.cstapin.auth.acceptance.QuizSteps.문제_생성;
-import static com.cstapin.auth.acceptance.QuizSteps.문제_생성_요청값;
+import static com.cstapin.auth.acceptance.QuizSteps.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -32,5 +31,18 @@ public class QuizDocumentation extends Documentation {
         //then
         Map<String, Object> request = 문제_생성_요청값(1L, "인덱스", "+++은 기본 인덱스이다.", List.of("pk", "기본키", "기본 키"));
         문제_생성(getRequestSpecification("admin-create-quiz"), request);
+    }
+
+    @Test
+    void findQuiz() {
+        //given
+        QuizResponse response = new QuizResponse(1L, "유기훈", 1L, "데이터베이스",
+                1L, "인덱스", "+++은 기본 인덱스이다.", List.of("pk", "기본키", "기본 키"), LocalDateTime.now());
+
+        //when
+        when(quizAdminService.findQuiz(any())).thenReturn(response);
+
+        //then
+        문제_상세_조회(getRequestSpecification("admin-find-quiz-details"), 1L);
     }
 }
