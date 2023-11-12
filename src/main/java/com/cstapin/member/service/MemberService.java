52159fc443d2f13f4689bb3b147f9285e0fa1dcf
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-11-12 12:27:25 +0900

    update: 프로필 조회 시에 하루 목표치, 당일 학습 완료 여부 내용 반환

--- src/main/java/com/cstapin/member/service/MemberService.java
+++ src/main/java/com/cstapin/member/service/MemberService.java
@@ -1,16 +1,20 @@
 package com.cstapin.member.service;
 
 
+import com.cstapin.member.domain.Member;
 import com.cstapin.member.service.dto.DailyGoalRequest;
 import com.cstapin.member.service.dto.MembersRequest;
 import com.cstapin.member.service.dto.MembersResponse;
 import com.cstapin.member.service.dto.ProfileResponse;
 import com.cstapin.member.service.query.MemberQueryService;
+import com.cstapin.quiz.service.query.LearningRecordQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @Slf4j
@@ -18,11 +22,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final LearningRecordQueryService learningRecordQueryService;
     private final MemberQueryService memberQueryService;
 
     public ProfileResponse findProfile(String username) {
-        // 학습 기록 기능 추가시키면서 completeQuizCount 값 추가할 예정
-        return ProfileResponse.of(memberQueryService.findByUsername(username), 0);
+        Member member = memberQueryService.findByUsername(username);
+
+        int completeQuizCount = learningRecordQueryService.findCompleteQuizCount(member.getId(), LocalDate.now());
+        boolean isDailyLearningComplete = learningRecordQueryService.isDailyLearningComplete(member.getId(), LocalDate.now());
+
+        return ProfileResponse.of(memberQueryService.findByUsername(username), completeQuizCount, isDailyLearningComplete);
     }
 
     @Transactional
