aa0a2418c610402c804df4f6ec1c203b6b2559ba
62hoon99 <ske06008@gmail.com>
2024-01-17 22:21:45 +0900

    update: 랜덤 퀴즈 참여 요청값 검증 추가

--- src/main/java/com/cstapin/quiz/service/dto/QuizParticipantsRequest.java
+++ src/main/java/com/cstapin/quiz/service/dto/QuizParticipantsRequest.java
@@ -3,20 +3,19 @@ package com.cstapin.quiz.service.dto;
 import com.cstapin.quiz.domain.QuizParticipants;
 import lombok.Getter;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Getter
 public class QuizParticipantsRequest {
 
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣]*$")
     @NotBlank
+    @Size(min = 2, max = 10)
     private String username;
 
     @Pattern(regexp = "^[0-9]*$")
     @NotBlank
+    @Size(min = 11, max = 11)
     private String phoneNumber;
 
     @Min(1)
